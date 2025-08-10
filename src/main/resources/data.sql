7c27c754afed229970f77f8b3002d21ad8119917
Kleber Vales <klebervales.dev@gmail.com>
2025-08-10 11:40:50 -0300

    chore: fixing database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO PRODUCT (name, price) VALUES ('Laptop', 3500.00);
-INSERT INTO PRODUCT (name, price) VALUES ('Mouse', 120.00);
-INSERT INTO PRODUCT (name, price) VALUES ('Teclado', 200.00);
+INSERT INTO PRODUCT (name, price) VALUES ('Mouse', 150.00);
