c3551fc09b48ff956dc593ef9f264a1d92d8e9fb
Kleber Vales <ksvales@outlook.pt>
2025-08-19 13:53:41 -0300

    chore: add coment

--- src/main/java/com/example/springdatajpademo/SpringDataJpaDemoApplication.java
+++ src/main/java/com/example/springdatajpademo/SpringDataJpaDemoApplication.java
@@ -8,4 +8,13 @@ public class SpringDataJpaDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaDemoApplication.class, args);
     }
+/*
+
+    - Aplicaçao para receber requisições HTTP (REST API ou MVC)
+    - Os repositórios JPA devem ficar disponiveis para operações CRUD
+    - Qualquer endpoint ou serviço configurado deve ficar acessivel em em http://localhost:8080
+    
+
+*/
+
 }
